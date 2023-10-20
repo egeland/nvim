9f28bc0f109598ef23f8229c08d274847a0f19b2
Frode Egeland <egeland@gmail.com>
2023-10-21 00:33:22 +1100

    initial commit, all my own
    Signed-off-by: Frode Egeland <egeland@gmail.com>

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    lazy = false,
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = {"lua", "vim", "vimdoc", "python", "rust", "bash", "dockerfile", "json", "make", "markdown", "markdown_inline", "nix", "requirements", "toml", "yaml"},
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
+
