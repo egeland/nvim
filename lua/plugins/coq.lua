2dfda571b991da26bf57ed29638578a8be21f436
Frode Egeland <egeland@gmail.com>
2023-10-21 08:54:01 +1100

    add coq and copilot (dep)
    Signed-off-by: Frode Egeland <egeland@gmail.com>

---
+++ lua/plugins/coq.lua
@@ -0,0 +1,16 @@
+return {
+  "ms-jpq/coq_nvim",
+  lazy = false,
+  build = ":COQnow",
+  dependencies = {
+    "ms-jpq/coq.artifacts",
+    "ms-jpq/coq.thirdparty",
+    "github/copilot.vim",
+  },
+  config = function()
+    require('coq_3p') {
+      { src = "nvimlua", short_name = "nLUA", conf_only = true },
+      { src = "copilot", short_name = "COP", accept_key = "<c-f>" }
+    }
+  end
+}
