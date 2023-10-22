d4054d7819996162ab0a81e24eb719f8fbec83e7
Frode Egeland <egeland@gmail.com>
2023-10-22 23:47:17 +1100

    add lsp config to mason
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,11 +4,20 @@ return {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
+    "simrat39/rust-tools.nvim",
   },
   config = function()
     require("mason").setup()
     require("mason-lspconfig").setup {
       ensure_installed = { "bashls", "dockerls", "jsonls", "lua_ls", "marksman", "pylsp", "rust_analyzer" },
+    }
+    require("mason-lspconfig").setup_handlers {
+        function (server_name)
+            require("lspconfig")[server_name].setup {}
+        end,
+        ["rust_analyzer"] = function ()
+            require("rust-tools").setup {}
+        end
     }
   end
 }
