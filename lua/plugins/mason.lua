1685b71b55d8f8f20d0f4e848cd9c189451d3486
Frode Egeland <egeland@gmail.com>
2023-10-22 21:17:51 +1100

    add lsp to mason
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,14 @@
 return {
   "williamboman/mason.nvim",
   lazy = false,
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
   config = function()
     require("mason").setup()
+    require("mason-lspconfig").setup {
+      ensure_installed = { "bashls", "dockerls", "jsonls", "lua_ls", "marksman", "pylsp", "rust_analyzer" },
+    }
   end
 }
