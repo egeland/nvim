3903e8d4a5cc4efb4353268038d4077ad2bd200f
Frode Egeland <egeland@gmail.com>
2023-10-26 22:35:29 +1100

    try other lsp
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,23 +1,63 @@
 return {
-  "williamboman/mason.nvim",
-  lazy = false,
+  'junnplus/lsp-setup.nvim',
   dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "simrat39/rust-tools.nvim",
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'williamboman/mason-lspconfig.nvim',
+    'simrat39/rust-tools.nvim',
   },
+  lazy = false,
   config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup {
-      ensure_installed = { "bashls", "dockerls", "jsonls", "lua_ls", "marksman", "pylsp", "rust_analyzer" },
-    }
-    require("mason-lspconfig").setup_handlers {
-        function (server_name)
-            require("lspconfig")[server_name].setup {}
+    require("mason").setup({
+      PATH = "prepend", -- "skip" seems to cause the spawning error
+    })
+    require('lsp-setup').setup({
+      servers = {
+        pylsp = {},
+        bashls = {},
+        dockerls = {},
+        rust_analyzer = function()
+          local rt = require('rust-tools')
+          rt.setup({
+            server = {
+              settings = {
+                ['rust-analyzer'] = {
+                  checkOnSave = {
+                    command = "clippy",
+                  },
+                  cargo = {
+                    loadOutDirsFromCheck = true,
+                  },
+                  procMacro = {
+                    enable = true,
+                  },
+                },
+              },
+              on_attach = function(_, bufnr)
+                -- Hover actions
+                vim.keymap.set("n", "<Leader>rh", rt.hover_actions.hover_actions, { buffer = bufnr })
+                -- Code action groups
+                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+              end,
+            },
+          })
         end,
-        ["rust_analyzer"] = function ()
-            require("rust-tools").setup {}
-        end
-    }
+        lua_ls = {
+          settings = {
+            Lua = {
+              hint = {
+                enable = false,
+                arrayIndex = "Auto",
+                await = true,
+                paramName = "All",
+                paramType = true,
+                semicolon = "SameLine",
+                setType = false,
+              },
+            },
+          },
+        },
+      }
+    })
   end
 }
