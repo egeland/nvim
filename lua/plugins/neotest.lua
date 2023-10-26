c4398039301f9d1b700503c82fafc9467578488a
Frode Egeland <egeland@gmail.com>
2023-10-26 20:04:59 +1100

    add neotest
    Signed-off-by: Frode Egeland <egeland@gmail.com>

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "antoinemadec/FixCursorHold.nvim",
+    "rouge8/neotest-rust",
+    "nvim-neotest/neotest-python"
+  },
+  event = {"BufRead *.rs", "BufRead *.py"},
+  config = function(_, opts)
+      require("neotest").setup {
+        adapters = {
+          require "neotest-rust",
+          require "neotest-python",
+          -- require "neotest-vim-test" {
+          --   ignore_file_types = { "python", "vim", "lua" },
+          -- },
+        },
+      }
+      vim.keymap.set('n', '<leader>t', "<CMD>Neotest run<CR>", { desc = 'Neotest Run' })
+      vim.keymap.set('n', '<leader>tr', "<CMD>Neotest run<CR>", { desc = 'Neotest Run' })
+      vim.keymap.set('n', '<leader>ts', "<CMD>Neotest summary<CR>", { desc = 'Neotest Summary' })
+    end,
+}
