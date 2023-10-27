84ce8b0e22aa74901cbf1d63a3dc278d3415e145
Frode Egeland <egeland@gmail.com>
2023-10-28 10:52:39 +1100

    tweak telescope
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,4 +2,19 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   event = "BufRead",
+  config = function()
+    local wk = require("which-key")
+    wk.register({
+      f = {
+        name = "[F]ile",
+        f = { "<cmd>Telescope find_files<cr>", "Find File" },
+        g = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+      },
+      b = {
+        name = "[B]uffers",
+        b = { "<cmd>Telescope buffers<cr>", "Buffers" },
+        d = { "<cmd>bdelete<cr>", "Delete Buffer" },
+      }
+    }, { prefix = "<leader>" })
+  end
 }
