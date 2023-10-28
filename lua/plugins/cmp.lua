f400666c91b115c8916fb08683239be9a9e27751
Frode Egeland <egeland@gmail.com>
2023-10-29 08:05:49 +1100

    tweak cmp
    Signed-off-by: Frode Egeland <egeland@gmail.com>

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -8,13 +8,14 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lua",
     "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
     "saadparwaiz1/cmp_luasnip",
   },
   lazy = false,
   priority = 100,
   config = function()
     local cmp = require('cmp')
-    -- vim.opt.completeopt = { "menu", "menuone", "noselect" }
+    vim.opt.completeopt = { "menu", "menuone", "noselect" }
     cmp.setup({
       snippet = {
         expand = function(args)
@@ -34,6 +35,7 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'nvim_lua' },
+        { name = 'nvim_lsp_signature_help' },
         { name = 'luasnip' },
         { name = 'crates' },
       }, {
@@ -55,11 +57,29 @@ return {
         { name = 'cmdline' }
       })
     })
-  -- Set up lspconfig.
-  local capabilities = require('cmp_nvim_lsp').default_capabilities()
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['lua_ls'].setup {
-    capabilities = capabilities
-  }
+    -- Set up lspconfig.
+    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+    require('lspconfig')['lua_ls'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['rust_analyzer'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['bashls'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['dockerls'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['jsonls'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['marksman'].setup {
+      capabilities = capabilities
+    }
+    require('lspconfig')['pylsp'].setup {
+      capabilities = capabilities
+    }
   end,
 }
